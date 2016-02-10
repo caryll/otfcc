b4ab53814461147e9e22f4aeefb3c2157959f555
Clerk Ma <clerkma@gmail.com>
2016-02-10 16:51:55 +0800

    table: 'maxp'.

--- otfcc.c
+++ otfcc.c
@@ -161,6 +161,24 @@ typedef struct {
 } opentype_table_name;
 
 typedef struct {
+  uint32_t table_version_number;
+  uint16_t numGlyphs;
+  uint16_t maxPoints;
+  uint16_t maxContours;
+  uint16_t maxCompositePoints;
+  uint16_t maxCompositeContours;
+  uint16_t maxZones;
+  uint16_t maxTwilightPoints;
+  uint16_t maxStorage;
+  uint16_t maxFunctionDefs;
+  uint16_t maxInstructionDefs;
+  uint16_t maxStackElements;
+  uint16_t maxSizeOfInstructions;
+  uint16_t maxComponentElements;
+  uint16_t maxComponentDepth;
+} opentype_table_maxp;
+
+typedef struct {
   int16_t Value;
   uint16_t DeviceTable;
 } opentype_math_value_record;
