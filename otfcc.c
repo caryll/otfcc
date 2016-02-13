2ecc91de21ca8618e5d174c1b006029ac9e102ba
Clerk Ma <clerkma@gmail.com>
2016-02-13 23:55:34 +0800

    TRUETYPE related tables.

--- otfcc.c
+++ otfcc.c
@@ -49,6 +49,8 @@ typedef enum {
     // 'COLR', 'CPAL'
 } opentype_table_type;
 
+/* Required Tables */
+
 typedef struct {
   uint32_t table_version_number;
   uint32_t fontRevison;
@@ -133,9 +135,18 @@ typedef struct {
 } opentype_horizontal_metric;
 
 typedef struct {
+  opentype_horizontal_matric * metrics;
+  int16_t * leftSideBearing;
+} opentypr_table_hmtx;
+
+typedef struct {
   uint16_t advanceHeight;
   int16_t  topSideBearing;
 } opentype_vertical_metric;
+
+typedef struct {
+  opentype_vertical_metric * metrics;
+} opentype_table_vmtx;
 
 typedef struct {
   uint16_t platformID;
@@ -177,152 +188,6 @@ typedef struct {
   uint16_t maxComponentElements;
   uint16_t maxComponentDepth;
 } opentype_table_maxp;
-
-typedef struct {
-  int16_t Value;
-  uint16_t DeviceTable;
-} opentype_math_value_record;
-
-typedef struct {
-  uint32_t Version;
-  uint16_t MathConstants;
-  uint16_t MathGlyphInfo;
-  uint16_t MathVariants;
-} opentype_table_math;
-
-typedef struct {
-  int16_t  ScriptPercentScaleDown;
-  int16_t  ScriptScriptPercentScaleDown;
-  uint16_t DelimitedSubFormulaMinHeight;
-  uint16_t DisplayOperatorMinHeight;
-  opentype_math_value_record MathLeading;
-  opentype_math_value_record AxisHeight;
-  opentype_math_value_record AccentBaseHeight;
-  opentype_math_value_record FlattenedAccentBaseHeight;
-  opentype_math_value_record SubscriptShiftDown;
-  opentype_math_value_record SubscriptTopMax;
-  opentype_math_value_record SubscriptBaselineDropMin;
-  opentype_math_value_record SuperscriptShiftUp;
-  opentype_math_value_record SuperscriptShiftUpCramped;
-  opentype_math_value_record SuperscriptBottomMin;
-  opentype_math_value_record SuperscriptBaselineDropMax;
-  opentype_math_value_record SubSuperscriptGapMin;
-  opentype_math_value_record SuperscriptBottomMaxWithSubscript;
-  opentype_math_value_record SpaceAfterScript;
-  opentype_math_value_record UpperLimitGapMin;
-  opentype_math_value_record UpperLimitBaselineRiseMin;
-  opentype_math_value_record LowerLimitGapMin;
-  opentype_math_value_record LowerLimitBaselineDropMin;
-  opentype_math_value_record StackTopShiftUp;
-  opentype_math_value_record StackTopDisplayStyleShiftUp;
-  opentype_math_value_record StackBottomShiftDown;
-  opentype_math_value_record StackBottomDisplayStyleShiftDown;
-  opentype_math_value_record StackGapMin;
-  opentype_math_value_record StackDisplayStyleGapMin;
-  opentype_math_value_record StretchStackTopShiftUp;
-  opentype_math_value_record StretchStackBottomShiftDown;
-  opentype_math_value_record StretchStackGapAboveMin;
-  opentype_math_value_record StretchStackGapBelowMin;
-  opentype_math_value_record FractionNumeratorShiftUp;
-  opentype_math_value_record FractionNumeratorDisplayStyleShiftUp;
-  opentype_math_value_record FractionDenominatorShiftDown;
-  opentype_math_value_record FractionDenominatorDisplayStyleShiftDown;
-  opentype_math_value_record FractionNumeratorGapMin;
-  opentype_math_value_record FractionNumDisplayStyleGapMin;
-  opentype_math_value_record FractionRuleThickness;
-  opentype_math_value_record FractionDenominatorGapMin;
-  opentype_math_value_record FractionDenomDisplayStyleGapMin;
-  opentype_math_value_record SkewedFractionHorizontalGap;
-  opentype_math_value_record SkewedFractionVerticalGap;
-  opentype_math_value_record OverbarVerticalGap;
-  opentype_math_value_record OverbarRuleThickness;
-  opentype_math_value_record OverbarExtraAscender;
-  opentype_math_value_record UnderbarVerticalGap;
-  opentype_math_value_record UnderbarRuleThickness;
-  opentype_math_value_record UnderbarExtraDescender;
-  opentype_math_value_record RadicalVerticalGap;
-  opentype_math_value_record RadicalDisplayStyleVerticalGap;
-  opentype_math_value_record RadicalRuleThickness;
-  opentype_math_value_record RadicalExtraAscender;
-  opentype_math_value_record RadicalKernBeforeDegree;
-  opentype_math_value_record RadicalKernAfterDegree;
-  int16_t RadicalDegreeBottomRaisePercent;
-} opentype_math_constants;
-
-typedef struct {
-  uint16_t MathItalicsCorrectionInfo;
-  uint16_t MathTopAccentAttachment;
-  uint16_t ExtendedShapeCoverage;
-  uint16_t MathKernInfo;
-} opentype_math_glyph_info;
-
-typedef struct {
-} opentype_math_variants;
-typedef struct {
-  uint16_t Coverage;
-  uint16_t ItalicsCorrectionCount;
-  opentype_math_value_record * ItalicsCorrection;
-} opentype_math_italics_correction_info;
-
-typedef struct {
-  uint16_t TopAccentCoverage;
-  uint16_t TopAccentAttachmentCount;
-  opentype_math_value_record * TopAccentAttachment;
-} opentype_math_top_accent_attachment;
-
-typedef struct {
-  uint16_t TopRightMathKern;
-  uint16_t TopLeftMathKern;
-  uint16_t BottomRightMathKern;
-  uint16_t BottomLeftMathKern;
-} opentype_math_kern_info_record;
-
-typedef struct {
-  uint16_t MathKernCoverage;
-  uint16_t MathKernCount;
-  opentype_math_kern_info_record * MathKernInfoRecords;
-} opentype_math_kern_info;
-
-typedef struct {
-  uint16_t HeightCount;
-  opentype_math_value_record * CorrectionHeight;
-  opentype_math_value_record * KernValue;
-} opentype_math_kern;
-
-typedef struct {
-  uint16_t MinConnectorOverlap;
-  uint16_t VertGlyphCoverage;
-  uint16_t HorizGlyphCoverage;
-  uint16_t VertGlyphCount;
-  uint16_t HorizGlyphCount;
-  uint16_t VertGlyphConstruction;
-  uint16_t HorizGlyphConstruction;
-} opentype_math_variants;
-
-typedef struct {
-  uint16_t VariantGlyph;
-  uint16_t AdvanceMeasurement;
-} opentype_math_glyph_variant_record;
-
-typedef struct {
-  uint16_t GlyphAssembly;
-  uint16_t VariantCount;
-  opentype_math_glyph_variant_record * MathGlyphVariantRecord;
-} opentype_math_glyph_construction;
-
-typedef struct {
-  uint16_t Glyph;
-  uint16_t StartConnectorLength;
-  uint16_t EndConnectorLength;
-  uint16_t FullAdvance;
-  uint16_t PartFlags;
-} opentype_glyph_part_record;
-
-typedef struct {
-  opentype_math_value_record ItalicsCorrection;
-  uint16_t PartCount;
-  opentype_glyph_part_record * PartRecords;
-} opentype_glyph_assembly;
 
 typedef struct {
   uint16_t version;
@@ -379,101 +244,6 @@ typedef struct {
   uint32_t cbSignature;
   uint8_t * bSignature;
 } opentype_table_dsig;
-
-typedef struct {
-  uint16_t majorVersion;
-  uint16_t minorVersion;
-  int16_t  defaultVertOriginY;
-  uint16_t numVertOriginYMetrics;
-} opentype_table_vorg;
-
-typedef struct {
-  uint16_t glyphIndex;
-  int16_t  vertOriginY;
-} opentype_vert_origin_y_metrics;
-
-typedef struct {
-  uint16_t version;
-  uint32_t offsetToSVGDocIndex;
-  uint32_t reserved;
-} opentype_table_svg;
-
-typedef struct {
-  uint16_t startGlyphID;
-  uint16_t endGlyphID;
-  uint32_t svgDocOffset;
-  uint32_t svgDocLength;
-} opentype_svg_document_index_entry;
-
-typedef struct {
-  uint16_t numEntries;
-  opentype_svg_document_index_entry * entries;
-} opentype_svg_document_index;
-
-typedef struct {
-  int32_t Version;
-  uint16_t HorizAxis;
-  uint16_t VertAxis;
-} opentype_table_base;
-
-typedef struct {
-  uint16_t BaseTagList;
-  uint16_t BaseScriptList;
-} opentype_axis_table;
-
-typedef struct {
-  uint16_t BaseTagCount;
-  uint32_t * BaselineTag;
-} opentype_base_tag_list_table;
-
-typedef struct {
-  uint32_t BaseScriptTag;
-  uint16_t BaseScript;
-} opentype_base_script_record;
-
-typedef struct {
-  uint16_t BaseScriptCount;
-  opentype_base_script_record * BaseScriptRecord;
-} opentype_base_script_list_table;
-
-typedef struct {
-  uint32_t BaseLangSysTag;
-  uint16_t MinMax;
-} opentype_base_lang_sys_record;
-
-typedef struct {
-  uint16_t BaseValues;
-  uint16_t DefaultMinMax;
-  uint16_t BaseLangSysCount;
-  opentype_base_lang_sys_record * BaseLangSysRecord;
-} opentype_base_script_table;
-
-typedef struct {
-  uint16_t DefaultIndex;
-  uint16_t BaseCoordCount;
-  uint16_t BaseCoord;
-} opentype_base_values_table;
-
-typedef struct {
-  uint32_t FeatureTableTag;
-  uint16_t MinCoord;
-  uint16_t MaxCoord;
-} opentype_feat_min_max_record;
-
-typedef struct {
-  uint16_t MinCoord;
-  uint16_t MaxCoord;
-  uint16_t FeatMinMaxCount;
-  opentype_feat_min_max_record * FeatMinMaxRecord;
-} opentype_min_max_table;
-
-typedef struct {
-  uint16_t BaseCoordFormat;
-  int16_t  Coordinate;
-  uint16_t ReferenceGlyph;
-  uint16_t BaseCoordPoint;
-  uint16_t DeviceTable;
-} opentype_base_coord;
 
 typedef struct {
   uint16_t version;
@@ -605,6 +375,195 @@ typedef struct {
   uint16_t glyphID;
 } opentype_num_uvs_mappings;
 
+/* Tables Related to TrueType Outlines */
+
+typedef struct {
+  int16_t * blob;
+  size_t size;
+} opentype_table_cvt;
+
+typedef struct {
+  uint8_t * blob;
+  size_t size;
+} opentype_table_fpgm;
+
+typedef struct {
+  union {
+    uint16_t * blob2;
+    uint32_t * blob4;
+  };
+  size_t size;
+} opentype_table_loca;
+
+typedef struct {
+  uint8_t * blob;
+  size_t size;
+} opentype_table_prep;
+
+enum {
+  GASP_GRIDFIT              = 0x0001,
+  GASP_DOGRAY               = 0x0002,
+  GASP_SYMMETRIC_GRIDFIT    = 0x0004,
+  GASP_SYMMETRIC_SMOOTHING  = 0x0008,
+};
+
+typedef struct {
+  uint16_t max_ppem;
+  uint16_t gasp_behavior;
+} gasp_range;
+
+typedef struct {
+  uint16_t numRanges;
+  gasp_range * gaspRange;
+} opentype_table_gasp;
+
+typedef struct {
+  uint16_t * endPtsOfContours;
+  uint16_t  instructionLength;
+  uint8_t * instructions;
+  uint8_t * flags;
+  uint16_t * xCoordinates;
+  uint16_t * yCoordinates;
+} simple_glyph;
+
+typedef struct {
+  uint16_t flags;
+  uint16_t glyphIndex;
+  uint16_t argument1;
+  uint16_t argument2;
+  uint8_t * trans;
+} composite_glyph;
+
+typedef struct {
+  int16_t numberOfContours;
+  int16_t xMin;
+  int16_t yMin;
+  int16_t xMax;
+  int16_t yMax;
+  union {
+    simple_glyph g1;
+    composite_glyph g2;
+  };
+} truetype_glyph;
+
+typedef struct {
+  truetype_glyph * glyphs;
+} opentype_table_glyf;
+
+/* Tables Related to PostScript Outlines */
+
+typedef struct {
+  uint8_t * blob;
+  size_t size;
+} opentype_table_cff;
+
+typedef struct {
+  uint16_t glyphIndex;
+  int16_t  vertOriginY;
+} opentype_vert_origin_y_metrics;
+
+typedef struct {
+  uint16_t majorVersion;
+  uint16_t minorVersion;
+  int16_t  defaultVertOriginY;
+  uint16_t numVertOriginYMetrics;
+  opentype_vert_origin_y_metrics * metrics;
+} opentype_table_vorg;
+
+/* Table related to SVG outlines */
+
+typedef struct {
+  uint16_t version;
+  uint32_t offsetToSVGDocIndex;
+  uint32_t reserved;
+} opentype_table_svg;
+
+typedef struct {
+  uint16_t startGlyphID;
+  uint16_t endGlyphID;
+  uint32_t svgDocOffset;
+  uint32_t svgDocLength;
+} opentype_svg_document_index_entry;
+
+typedef struct {
+  uint16_t numEntries;
+  opentype_svg_document_index_entry * entries;
+} opentype_svg_document_index;
+
+/* Advanced Typographic Tables */
+
+typedef struct {
+  int32_t Version;
+  uint16_t HorizAxis;
+  uint16_t VertAxis;
+} opentype_table_base;
+
+typedef struct {
+  uint16_t BaseTagList;
+  uint16_t BaseScriptList;
+} opentype_axis_table;
+
+typedef struct {
+  uint16_t BaseTagCount;
+  uint32_t * BaselineTag;
+} opentype_base_tag_list_table;
+
+typedef struct {
+  uint32_t BaseScriptTag;
+  uint16_t BaseScript;
+} opentype_base_script_record;
+
+typedef struct {
+  uint16_t BaseScriptCount;
+  opentype_base_script_record * BaseScriptRecord;
+} opentype_base_script_list_table;
+
+typedef struct {
+  uint32_t BaseLangSysTag;
+  uint16_t MinMax;
+} opentype_base_lang_sys_record;
+
+typedef struct {
+  uint16_t BaseValues;
+  uint16_t DefaultMinMax;
+  uint16_t BaseLangSysCount;
+  opentype_base_lang_sys_record * BaseLangSysRecord;
+} opentype_base_script_table;
+
+typedef struct {
+  uint16_t DefaultIndex;
+  uint16_t BaseCoordCount;
+  uint16_t BaseCoord;
+} opentype_base_values_table;
+
+typedef struct {
+  uint32_t FeatureTableTag;
+  uint16_t MinCoord;
+  uint16_t MaxCoord;
+} opentype_feat_min_max_record;
+
+typedef struct {
+  uint16_t MinCoord;
+  uint16_t MaxCoord;
+  uint16_t FeatMinMaxCount;
+  opentype_feat_min_max_record * FeatMinMaxRecord;
+} opentype_min_max_table;
+
+typedef struct {
+  uint16_t BaseCoordFormat;
+  int16_t  Coordinate;
+  uint16_t ReferenceGlyph;
+  uint16_t BaseCoordPoint;
+  uint16_t DeviceTable;
+} opentype_base_coord;
+
+enum {
+  GLYPH_BASE = 1,
+  GLYPH_LIGATURE,
+  GLYPH_MARK,
+  GLYPH_COMPONENT
+};
+
 typedef struct {
   uint32_t JstfScriptTag;
   uint16_t JstfScript;
@@ -666,22 +625,149 @@ typedef struct {
   uint16_t Lookup;
 } opentype_jstf_max;
 
-/*
 typedef struct {
-  header;
-  name_index;
-  top_dict_index;
-  string_index;
-  global_subr_index;
-  encodings;
-  charsets;
-  fdselect;
-  charstring_index;
-  font_dict_index;
-  private_dict;
-  local_subr_index;
-} opentype_table_cff;
-*/
+  int16_t Value;
+  uint16_t DeviceTable;
+} opentype_math_value_record;
+
+typedef struct {
+  uint32_t Version;
+  uint16_t MathConstants;
+  uint16_t MathGlyphInfo;
+  uint16_t MathVariants;
+} opentype_table_math;
+
+typedef struct {
+  int16_t  ScriptPercentScaleDown;
+  int16_t  ScriptScriptPercentScaleDown;
+  uint16_t DelimitedSubFormulaMinHeight;
+  uint16_t DisplayOperatorMinHeight;
+  opentype_math_value_record MathLeading;
+  opentype_math_value_record AxisHeight;
+  opentype_math_value_record AccentBaseHeight;
+  opentype_math_value_record FlattenedAccentBaseHeight;
+  opentype_math_value_record SubscriptShiftDown;
+  opentype_math_value_record SubscriptTopMax;
+  opentype_math_value_record SubscriptBaselineDropMin;
+  opentype_math_value_record SuperscriptShiftUp;
+  opentype_math_value_record SuperscriptShiftUpCramped;
+  opentype_math_value_record SuperscriptBottomMin;
+  opentype_math_value_record SuperscriptBaselineDropMax;
+  opentype_math_value_record SubSuperscriptGapMin;
+  opentype_math_value_record SuperscriptBottomMaxWithSubscript;
+  opentype_math_value_record SpaceAfterScript;
+  opentype_math_value_record UpperLimitGapMin;
+  opentype_math_value_record UpperLimitBaselineRiseMin;
+  opentype_math_value_record LowerLimitGapMin;
+  opentype_math_value_record LowerLimitBaselineDropMin;
+  opentype_math_value_record StackTopShiftUp;
+  opentype_math_value_record StackTopDisplayStyleShiftUp;
+  opentype_math_value_record StackBottomShiftDown;
+  opentype_math_value_record StackBottomDisplayStyleShiftDown;
+  opentype_math_value_record StackGapMin;
+  opentype_math_value_record StackDisplayStyleGapMin;
+  opentype_math_value_record StretchStackTopShiftUp;
+  opentype_math_value_record StretchStackBottomShiftDown;
+  opentype_math_value_record StretchStackGapAboveMin;
+  opentype_math_value_record StretchStackGapBelowMin;
+  opentype_math_value_record FractionNumeratorShiftUp;
+  opentype_math_value_record FractionNumeratorDisplayStyleShiftUp;
+  opentype_math_value_record FractionDenominatorShiftDown;
+  opentype_math_value_record FractionDenominatorDisplayStyleShiftDown;
+  opentype_math_value_record FractionNumeratorGapMin;
+  opentype_math_value_record FractionNumDisplayStyleGapMin;
+  opentype_math_value_record FractionRuleThickness;
+  opentype_math_value_record FractionDenominatorGapMin;
+  opentype_math_value_record FractionDenomDisplayStyleGapMin;
+  opentype_math_value_record SkewedFractionHorizontalGap;
+  opentype_math_value_record SkewedFractionVerticalGap;
+  opentype_math_value_record OverbarVerticalGap;
+  opentype_math_value_record OverbarRuleThickness;
+  opentype_math_value_record OverbarExtraAscender;
+  opentype_math_value_record UnderbarVerticalGap;
+  opentype_math_value_record UnderbarRuleThickness;
+  opentype_math_value_record UnderbarExtraDescender;
+  opentype_math_value_record RadicalVerticalGap;
+  opentype_math_value_record RadicalDisplayStyleVerticalGap;
+  opentype_math_value_record RadicalRuleThickness;
+  opentype_math_value_record RadicalExtraAscender;
+  opentype_math_value_record RadicalKernBeforeDegree;
+  opentype_math_value_record RadicalKernAfterDegree;
+  int16_t RadicalDegreeBottomRaisePercent;
+} opentype_math_constants;
+
+typedef struct {
+  uint16_t MathItalicsCorrectionInfo;
+  uint16_t MathTopAccentAttachment;
+  uint16_t ExtendedShapeCoverage;
+  uint16_t MathKernInfo;
+} opentype_math_glyph_info;
+
+typedef struct {
+  uint16_t Coverage;
+  uint16_t ItalicsCorrectionCount;
+  opentype_math_value_record * ItalicsCorrection;
+} opentype_math_italics_correction_info;
+
+typedef struct {
+  uint16_t TopAccentCoverage;
+  uint16_t TopAccentAttachmentCount;
+  opentype_math_value_record * TopAccentAttachment;
+} opentype_math_top_accent_attachment;
+
+typedef struct {
+  uint16_t TopRightMathKern;
+  uint16_t TopLeftMathKern;
+  uint16_t BottomRightMathKern;
+  uint16_t BottomLeftMathKern;
+} opentype_math_kern_info_record;
+
+typedef struct {
+  uint16_t MathKernCoverage;
+  uint16_t MathKernCount;
+  opentype_math_kern_info_record * MathKernInfoRecords;
+} opentype_math_kern_info;
+
+typedef struct {
+  uint16_t HeightCount;
+  opentype_math_value_record * CorrectionHeight;
+  opentype_math_value_record * KernValue;
+} opentype_math_kern;
+
+typedef struct {
+  uint16_t MinConnectorOverlap;
+  uint16_t VertGlyphCoverage;
+  uint16_t HorizGlyphCoverage;
+  uint16_t VertGlyphCount;
+  uint16_t HorizGlyphCount;
+  uint16_t VertGlyphConstruction;
+  uint16_t HorizGlyphConstruction;
+} opentype_math_variants;
+
+typedef struct {
+  uint16_t VariantGlyph;
+  uint16_t AdvanceMeasurement;
+} opentype_math_glyph_variant_record;
+
+typedef struct {
+  uint16_t GlyphAssembly;
+  uint16_t VariantCount;
+  opentype_math_glyph_variant_record * MathGlyphVariantRecord;
+} opentype_math_glyph_construction;
+
+typedef struct {
+  uint16_t Glyph;
+  uint16_t StartConnectorLength;
+  uint16_t EndConnectorLength;
+  uint16_t FullAdvance;
+  uint16_t PartFlags;
+} opentype_glyph_part_record;
+
+typedef struct {
+  opentype_math_value_record ItalicsCorrection;
+  uint16_t PartCount;
+  opentype_glyph_part_record * PartRecords;
+} opentype_glyph_assembly;
 
 int main (void)
 {
