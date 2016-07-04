3bf2fc3f91ae22e4122c6b89b0dff996a2134209
clerkma <clerkma@gmail.com>
2016-07-04 20:35:23 +0800

    modified CFF.c/CFF.h.

--- src/tables/CFF.c
+++ src/tables/CFF.c
@@ -1,8 +1,13 @@
 #include "CFF.h"
 
-/*
-table_cff *caryll_read_cff(caryll_packet packet, uint32_t tag) {
-  table_cff *t = NULL;
+caryll_cff_parse_result *caryll_read_CFF_and_glyf(caryll_packet packet, uint32_t tag)
+{
+  caryll_cff_parse_result *t = NULL;
+  return NULL;
+}
+
+table_CFF *caryll_read_cff(caryll_packet packet, uint32_t tag) {
+  table_CFF *t = NULL;
   FOR_TABLE(tag, table) {
     font_file_pointer data = table.data;
     uint32_t length = table.length;
@@ -10,20 +15,19 @@ table_cff *caryll_read_cff(caryll_packet packet, uint32_t tag) {
   return NULL;
 }
 
-void caryll_delete_cff(table_cff *table) {
+void caryll_delete_CFF(table_CFF *table) {
   if (!table) return;
 }
 
-void caryll_cff_to_json(table_cff *table, json_value *root, caryll_dump_options *dumpopts,
+void caryll_CFF_to_json(table_CFF *table, json_value *root, caryll_dump_options *dumpopts,
 			const char *tag) {
   if (!table) return;
 }
 
-table_cff *caryll_cff_from_json(json_value *root, const tag *tag) {
+table_CFF *caryll_CFF_from_json(json_value *root, caryll_dump_options *dumpopts) {
   return NULL;
 }
 
-caryll_buffer *caryll_write_cff(table *table) {
+caryll_buffer *caryll_write_CFF(caryll_cff_parse_result cffAndGlyf) {
   return NULL;
 }
-*/
