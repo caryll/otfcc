c90a422972f18d56b5697ee3c8d2de4feaccc404
be5invis <belleve@typeof.net>
2016-07-13 06:22:11 +0800

    remove redundent includedirs

--- premake5.lua
+++ premake5.lua
@@ -31,7 +31,7 @@ workspace "otfcc"
 
 	filter "action:xcode4"
 		location "build/xcode"
-		includedirs { "dep/extern", "dep/extern/**", "lib/**" }
+		--includedirs { "dep/extern", "dep/extern/**", "lib/**" }
 		buildoptions { '-std=gnu11', '-Wall', '-Wno-multichar' }
 	filter {}
 	
