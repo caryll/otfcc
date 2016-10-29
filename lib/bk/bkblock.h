e7ccd8a43d372887d597a10ed30b81263df305b7
be5invis <belleve@typeof.net>
2016-10-29 21:06:23 +0800

    it is no longer necessary

--- lib/bk/bkblock.h
+++ lib/bk/bkblock.h
@@ -11,17 +11,16 @@
 
 struct __caryll_bkblock;
 typedef enum {
-	bkover = 0,     // nothing
-	b8 = 1,         // byte
-	b16 = 2,        // short
-	b32 = 3,        // long
-	p16 = 0x10,     // 16-bit offset, p = pointer to block
-	p32 = 0x11,     // 32-bit offset, p = pointer to block
-	sp16 = 0x80,    // 16-bit offset, p = pointer to block, marked as compact
-	sp32 = 0x81,    // 32-bit offset, p = pointer to block, marked as compact
-	bkcopy = 0xFE,  // Embed another block
-	bkembed = 0xFF, // Embed another block
-	__bk_force_width = INT_MAX
+	bkover = 0,    // nothing
+	b8 = 1,        // byte
+	b16 = 2,       // short
+	b32 = 3,       // long
+	p16 = 0x10,    // 16-bit offset, p = pointer to block
+	p32 = 0x11,    // 32-bit offset, p = pointer to block
+	sp16 = 0x80,   // 16-bit offset, p = pointer to block, marked as compact
+	sp32 = 0x81,   // 32-bit offset, p = pointer to block, marked as compact
+	bkcopy = 0xFE, // Embed another block
+	bkembed = 0xFF // Embed another block
 } bk_CellType;
 typedef enum { VISIT_WHITE, VISIT_GRAY, VISIT_BLACK } bk_cell_visit_state;
 
