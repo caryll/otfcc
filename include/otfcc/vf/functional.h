2f91fe7726b095856ff8c7af15f7ea7221f3bc07
be5invis <belleve@typeof.net>
2016-11-04 17:07:44 +0800

    we do not have any constant form now

--- include/otfcc/vf/functional.h
+++ include/otfcc/vf/functional.h
@@ -11,7 +11,7 @@ typedef enum {
 	vf_plus = 2,     // <plus z1 z2 | x> = <z1 | x> + <z2 | x>
 	vf_minus = 3,    // <minus z1 z2 | x> = <z1 | x> - <z2 | x>
 	vf_multiply = 4, // <multiply z1 z2 | x> = <z1 | s> <z2 | x>
-	vf_gxblend = 5   // <gxblend d1 d2 ... dn | w1 w2 ... wn> = <x | w> + <d1 | w> * w1 + ... + <dn | w> * wn;
+	vf_gxblend = 5   // <gxblend d1 d2 ... dn | w1 w2 ... wn> = <d1 | w> * w1 + ... + <dn | w> * wn;
 } VF_OPERATOR;
 
 typedef struct vf_Functional {
