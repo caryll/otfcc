36bfd594a44766440f3d9e4ca04f4a1963d53377
be5invis <belleve@typeof.net>
2017-03-04 21:51:06 +0800

    r0.7.0

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ require "dep/premake-modules/xcode-alt"
 require "dep/premake-modules/ninja"
 
 MAIN_VER = '0'
-SECONDARY_VER = '6'
-PATCH_VER = '3'
+SECONDARY_VER = '7'
+PATCH_VER = '0'
 
 function cbuildoptions()
 	-- Windows
