c86722cd23a7d77741ad203be9dd5841496e3edc
be5invis <belleve@typeof.net>
2017-11-10 15:48:55 +0800

    add typedef and implementation of vf_Axis and vf_Axes

--- include/otfcc/vf/axis.h
+++ include/otfcc/vf/axis.h
@@ -1,6 +1,8 @@
 #ifndef CARYLL_VF_AXIS_H
 #define CARYLL_VF_AXIS_H
 
+#include "caryll/element.h"
+#include "caryll/vector.h"
 #include "otfcc/primitives.h"
 
 typedef struct {
@@ -10,6 +12,10 @@ typedef struct {
 	pos_t maxValue;
 	uint16_t flags;
 	uint16_t axisNameID;
-} VF_Axis;
+} vf_Axis;
+
+extern caryll_ValElementInterface(vf_Axis) vf_iAxis;
+typedef caryll_Vector(vf_Axis) vf_Axes;
+extern caryll_VectorInterface(vf_Axes, vf_Axis) vf_iAxes;
 
 #endif
